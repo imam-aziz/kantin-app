c852b0071fc6371384188d47cef53f1a1437df2e
IMAM <imam76@upi.edu>
2026-05-06 18:05:39 +0700

    Fresh start

--- backend/app.py
+++ backend/app.py
@@ -13,25 +13,24 @@ kantin_data = {
     "menu": ["Nasi Goreng", "Es Teh", "Gorengan"]
 }
 
-@app.route('/api/menu', methods=['GET'])
+@app.route('/api/menu', methods=['GET']) # Sesuaikan dengan fetch di index.html
 def get_info():
-    # Menambahkan data identitas ke dalam response agar terbaca oleh index.html
     response = {
         "nama_kantin": kantin_data["nama_kantin"],
         "menu": kantin_data["menu"],
-        "pemilik": nama_owner,
-        "nim": nim_owner,
+        "pemilik": nama_owner, # Mengirim variabel dari os.environ
+        "nim": nim_owner,      # Mengirim variabel dari os.environ
         "judul_katalog": "Katalog Kantin - Praktikum 2"
     }
     return jsonify(response)
 
-@app.route('/api/menu/add-menu', methods=['POST'])
+@app.route('/api/menu/add-menu', methods=['POST']) # Sesuaikan dengan fetch tambahMenu
 def add_menu():
     new_item = request.json.get('item')
     if new_item:
         kantin_data["menu"].append(new_item)
-        return jsonify({"message": "Menu berhasil ditambah!", "menu": kantin_data["menu"]}), 201
-    return jsonify({"error": "Data tidak valid"}), 400
+        return jsonify({"message": "Berhasil", "menu": kantin_data["menu"]}), 201
+    return jsonify({"error": "Gagal"}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
